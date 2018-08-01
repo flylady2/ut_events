61549beea788549ff8680e1ded4a29b0a7a3e641
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-01 22:08:38 +0000

    ut_events and cli.rb code

---
+++ lib/ut_events/cli.rb
@@ -0,0 +1,16 @@
+class UtEvents::CLI
+
+  def start
+    puts "Welcome to UT Events"
+
+
+    #UtEvents::Scraper method
+    main_menu
+  end
+
+  def main_menu
+    puts "These are the events that are happening today"
+    puts ""
+    #displays a list of events
+    #Iterating through Event.all to display a numbered list with names
+end
