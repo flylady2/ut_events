d4bcc22fd84fe62859a0260e52029a4f84329367
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-01 22:22:43 +0000

    event.rb code

--- lib/ut_events/event.rb
+++ lib/ut_events/event.rb
@@ -1,1 +1,15 @@
+class UtEvents::Events
+  attr_accessor :name, :brief_description, :location, :affiliations
+  @@all = []
 
+  def initialize(name, brief_description, location, affiliations)
+    @name = name
+    @brief_description = brief_description
+    @location = location
+    @affiliations = affiliations
+  end
+
+  def self.all
+    @@all
+  end
+end
