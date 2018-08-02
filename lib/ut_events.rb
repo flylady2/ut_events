ea2cf38eca897d0d9c23fe63b195035f653df415
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-02 18:36:10 +0000

    adding requirements

--- lib/ut_events.rb
+++ lib/ut_events.rb
@@ -3,3 +3,11 @@ require "ut_events/version"
 module UtEvents
   # Your code goes here...
 end
+
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require_relative "./ut_events/version"
+require_relative "./ut_events/cli"
+require_relative "./ut_events/scraper"
+require_relative ".ut_events/event"
