ffc051f7b5a6eb069959ed397bd3755537de15ee
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-03 20:57:23 +0000

    code for find method with help from Kenlyn Terai

--- lib/ut_events/event.rb
+++ lib/ut_events/event.rb
@@ -25,15 +25,21 @@ class UtEvents::Event
     UtEvents::Scraper.scrape_events.each do |item|
 
       item.each do |key, value|
-        #binding.pry
+        binding.pry
         if key == :affiliations && value.class == Array && value.include?(category)
           selected_events << item
         end
       end
     end
     selected_events
-    
+
   end
+
+  # UtEvents::Event.all.collect do |object|
+  #   if object.affiliations.include?(category)
+  # =>  object
+  # => end
+  # end
 
 
 end
