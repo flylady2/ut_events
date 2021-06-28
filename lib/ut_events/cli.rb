58a07e042eee0584a1c239204d82367a2f8950da
Leslie Stevens <flymaven@protonmail.com>
2021-06-28 17:45:11 -0500

    add menu options

--- lib/ut_events/cli.rb
+++ lib/ut_events/cli.rb
@@ -21,7 +21,7 @@ class UtEvents::CLI
 
   def main_menu
     puts "To view additional details about a particular event, type in its number from the list."
-    puts "Type the letter C to see a list of event categories."
+    puts "Type 'Cat' to see a list of event categories."
     puts "Or type 'exit' to quit the program."
     user_input = ""
     user_input = gets.strip
@@ -30,7 +30,7 @@ class UtEvents::CLI
     elsif user_input.to_i.between?(1, UtEvents::Event.all.size)
       index = user_input.to_i - 1
       chosen_event(index)
-    elsif user_input == "C" || user_input == "c"
+    elsif user_input == "Cat" || user_input == "cat"
       category_menu
     else
       puts "Please try again"
@@ -83,22 +83,86 @@ class UtEvents::CLI
     last_input = gets.strip.downcase
 
     case last_input
-    when "a"
-      UtEvents::Event.find_by_category("Research Opportunities & Studies").each do |event| #iterates through the new array to print out the event names
+    when "ac"
+      UtEvents::Event.find_by_category("Academics").each do |event| #iterates through the new array to print out the event names
+        puts "#{event.name}"
+          end
+    when "ar"
+      UtEvents::Event.find_by_category("Arts & Humanities").each do |event| #iterates through the new array to print out the event names
         puts "#{event.name}"
           end
     when "b"
-      UtEvents::Event.find_by_category("Health & Wellness").each do |event|
+      UtEvents::Event.find_by_category("Business & Economy").each do |event| #iterates through the new array to print out the event names
         puts "#{event.name}"
-      end
+          end
     when "c"
       UtEvents::Event.find_by_category("Campus & Community").each do |event|
+        #puts "#{event.name}"
+        display_event(event)
+      end
+      second_look()
+    when "d"
+      UtEvents::Event.find_by_category("Diversity").each do |event|
+        puts "#{event.name}"
+      end
+    when "h"
+      UtEvents::Event.find_by_category("Health & Wellness").each do |event|
+        puts "Event"
+        puts "-#{event.name}"
+        puts "-#{event.location}"
+        puts "-#{event.description}"
+        puts "-url: #{event.event_link}"
+        puts "---------------------------"
+      end
+    when "p"
+      UtEvents::Event.find_by_category("Policy & Law").each do |event|
+        puts "#{event.name}"
+      end
+    when "r"
+      UtEvents::Event.find_by_category("Research Opportunities & Studies").each do |event|
+        puts "#{event.name}"
+      end
+    when "sci"
+      UtEvents::Event.find_by_category("Science & Tech").each do |event|
+        puts "#{event.name}"
+      end
+    when "soc"
+      UtEvents::Event.find_by_category("Social Justice").each do |event|
+        puts "#{event.name}"
+      end
+    when "spo"
+      UtEvents::Event.find_by_category("Sports & Recreation").each do |event|
+        puts "#{event.name}"
+      end
+    when "w"
+      UtEvents::Event.find_by_category("World & Culture").each do |event|
         puts "#{event.name}"
       end
     when "again"
       display_menu
     when 'done'
       puts "Goodbye y'all!"
+    end
+  end
+
+  def display_event(event)
+    puts "********Event**************"
+    puts "-#{event.name}"
+    puts "-#{event.location}"
+    puts "-#{event.description}"
+    puts "-url: #{event.event_link}"
+    puts "---------------------------"
+  end
+
+  def second_look
+    puts "Do you want to see all the events again?"
+    puts "Type y for yes, n to exit the program"
+    user_input = ""
+    user_input = gets.strip
+    if user_input == 'n' || user_input == 'N'
+      puts "Goodbye y'all!"
+    else
+      main_menu
     end
   end
 
